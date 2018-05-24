851aa676b12394a1e6caa19379714378d1455849
xwings <kj@xandora.net>
2018-05-24 14:58:31 +0000

    add mounting script

---
+++ scripts/mount.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+model="$1"
+modeldir="$opt/$model"
+
+if [ ! -d $modeldir ]; then
+    echo "model not found"
+    exit 1
+fi
+
+cd $modeldir
+
+if [ ! -d $modeldir/proc ] || [ ! -d $modeldir/sys ] || [ ! -d $modeldir/dev ]; then
+    echo "create proc sys dev"
+    mkdir proc sys dev
+fi
+
+mount -t proc proc proc/
+mount --rbind /sys sys/
+mount --rbind /dev dev/
